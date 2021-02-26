043b3df7b36bc527a7a0b617a820817a29538a59
Nil1510 <nilbajaniya@gmail.com>
2021-02-26 15:45:33 -0500

    Book table Author records

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 require('mysqli_connect.php');
 
-$booksQuery = "SELECT * FROM bookinventory";
+$booksQuery = "SELECT bi.BookId, bi.BookName, bi.Price, bi.NoOfPages, bi.PublishDate, CONCAT(GROUP_CONCAT(' ',a.Name)) AS Authors
+               FROM bookinventory bi LEFT JOIN author a 
+               ON  bi.BookId = a.BookId
+               GROUP BY bi.BookId
+               ORDER BY bi.PublishDate DESC";
+
 $books = mysqli_query($dbc, $booksQuery) or die(mysqli_error($dbc)); 
 ?>
 
@@ -11,27 +16,37 @@ $books = mysqli_query($dbc, $booksQuery) or die(mysqli_error($dbc));
 <head>
 <title> BookStore: Home </title>
 
+<style>
+    th, td{
+        padding: 10px;
+    }
+</style>
 </head>
 
 <body>
-<table>
+<table border="2" >
     <thead>
         <tr>
             <th> Title </th>
-            <th> Price </th>
+            <th> Authors </th>
+            <th> Publish Date </th>
+            <th> Price (CAD) </th>
             <th> Pages </th>
         </tr>
     </thead>
 
     <tbody>
         <?php 
+            //$bookIndex = 0;
             while($book = mysqli_fetch_array($books))
             {
                 echo "<tr>";
 
                 echo "<td>{$book['BookName']}</td>";
-                echo "<td>{$book['Price']}</td>";
-                echo "<td>{$book['Pages']}</td>";
+                echo "<td>{$book['Authors']}</td>";
+                echo "<td>{$book['PublishDate']}</td>";
+                echo "<td>$ {$book['Price']}</td>";
+                echo "<td>{$book['NoOfPages']}</td>";
 
                 echo "</tr>";
             }
---
+++ sql/InsertAuthor.sql
@@ -0,0 +1,11 @@
+INSERT INTO `bookstore`.`author`
+(`authorId`,
+`Name`,
+`BookId`)
+VALUES
+(0, 'James Bond', 1),
+(0, 'Jhon Wick', 1),
+(0, 'Robert Jr', 2),
+(0, 'Hanry Cavins', 3),
+(0, 'Bill Gates', 4),
+(0, 'Steve Jobs', 5);
